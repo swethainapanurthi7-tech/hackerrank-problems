79091f7bce2511e0b28a82e7fb54e85808da4e03
swethainapanurthi7-tech <swethainapanurthi7@gmail.com>
2026-01-12 22:41:11 +0530

    Add files via upload

---
+++ exceptions.py
@@ -0,0 +1,11 @@
+ Number of test cases
+t = int(input())
+
+for _ in range(t):
+    try:
+        a, b = input().split()
+        print(int(a) // int(b))
+    except ZeroDivisionError as e:
+        print("Error Code:", e)
+    except ValueError as e:
+        print("Error Code:", e)
